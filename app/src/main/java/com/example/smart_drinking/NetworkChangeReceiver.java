7836b85a9329e10d133eca907e38fb9711dc3cc8
KarlaD10 <l18290905@cdguzman.tecnm.mx>
2024-04-05 00:28:12 -0600

    Notificaciones

--- app/src/main/java/com/example/smart_drinking/NetworkChangeReceiver.java
+++ app/src/main/java/com/example/smart_drinking/NetworkChangeReceiver.java
@@ -20,23 +20,10 @@ import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.smart_drinking.DataBase.DataHelper;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
     DataHelper mydb;
